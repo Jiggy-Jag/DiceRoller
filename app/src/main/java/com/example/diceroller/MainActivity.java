49fdbd06c375ffcfd3a1da607ee9adeb21cf3f93
Jiggy-Jag <56381845+Jiggy-Jag@users.noreply.github.com>
2019-11-07 17:10:41 +0000

    -Added second button

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -13,10 +13,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    int correct_counter = 1 ;
+    int correct_counter = 1;
+
+    public static int Roll_the_dice() {
+        Random r = new Random();
+        int number = r.nextInt(6);
+        return number;
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,23 +65,49 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
     public void on_button_click(View view) {
+        int Roll = Roll_the_dice();
         TextView tv3 = this.findViewById(R.id.textView3);
         TextView et = this.findViewById(R.id.editText);
         TextView tv = this.findViewById(R.id.textView2);
-        Random r = new Random();
-        int number = r.nextInt(6);
-        tv.setText(Integer.toString(number));
-        String value= et.getText().toString();
-        int Fvalue= Integer.parseInt(value);
 
-        if(number == Fvalue) {
+        tv.setText(Integer.toString(Roll));
+        String value = et.getText().toString();
+        int Fvalue = Integer.parseInt(value);
 
-            tv.setText(String.valueOf(number)+ " " + "Congratulations!");
+        if (Roll == Fvalue) {
+
+            tv.setText(String.valueOf(Roll) + " " + "Congratulations!");
             tv3.setText(String.valueOf(correct_counter));
             correct_counter++;
+        }
+
+
     }
 
+    public void on_button_click_icebreaker(View view) {
+        int Roll = Roll_the_dice();
+
+        TextView tv = this.findViewById(R.id.textView2);
+
+        if (Roll == 1)
+            tv.setText("If you could go anywhere in the world, where would you go?");
+        if (Roll == 2)
+                tv.setText("If you were stranded on a desert island, what three things would you want to take with you?");
+        if (Roll == 3)
+                tv.setText("If you could eat only one food for the rest of your life, what would that be?");
+        if (Roll == 4)
+                tv.setText("If you won a million dollars, what is the first thing you would buy?");
+        if (Roll == 5)
+                tv.setText("If you could spaned the day with one fictional character, who would it be?");
+        if (Roll == 6)
+                tv.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+
+
+
+    }
+    public void on_button_click_Finish(View view) {
 
     }
 }
